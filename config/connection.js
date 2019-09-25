4ac00b828587d676973931ec7fdee33c96978856
Christina <christinachon90@gmail.com>
2019-09-24 21:18:50 -0700

    added jawsdb info

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,13 @@ var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection({
+    host: 'rtzsaka6vivj2zp1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'ps814t3rohr3eya6',
+    password: 'ikx6vetboimati0m',
+    database: 'h6d65pmqz7j0slkk'
+  });
 }
 else {
   connection = mysql.createConnection({
